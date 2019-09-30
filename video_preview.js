1d56a3886fe7329deed1f2e13d25f8af77fc5863
unknown <ybandar1902@gmail.com>
2019-09-30 17:41:25 +0100

    Implements function to hide video
    - video thumbnail is previewed instead

--- video_preview.js
+++ video_preview.js
@@ -1,33 +1,45 @@
-let getVideos = function(){
-    let videos = [... document.getElementsByTagName("video-preview")];  
+let getVideos = function () {
+    let videos = [...document.getElementsByTagName("video-preview")];
     return videos;
 }
 
-let addHoverListeners = function(elements){
-    elements.map(element => 
-        element.addEventListener("mouseover",videoPlay)
+let hideVideos = function (elements) {
+    elements.map(function (element) {
+        let video = [...element.getElementsByTagName("video")];
+
+        if (video.length < 1) return;
+        video = video[0];
+
+        video.hidden = true;
+    }
     );
 }
 
-let addThumbnails = function(elements){
+let addHoverListeners = function (elements) {
+    elements.map(element =>
+        element.addEventListener("mouseover", videoPlay)
+    );
+}
+
+let addThumbnails = function (elements) {
     elements.map(element =>
         renderThumbnail(element));
 }
 
 
-let renderThumbnail = function(videoPreview){
-    let video = [... videoPreview.getElementsByTagName("video")];
-    
-    if(video.length < 1) return;
+let renderThumbnail = function (videoPreview) {
+    let video = [...videoPreview.getElementsByTagName("video")];
+
+    if (video.length < 1) return;
     video = video[0];
-    
+
     let frame = videoPreview.getAttribute("thumbnail-frame");
     let sec = frame != null ? frame : 0;
 
     let src = video.getAttribute("src");
-    if(src != null){
-        getVideoImage(src,sec,function(img){
-            if(img){
+    if (src != null) {
+        getVideoImage(src, sec, function (img) {
+            if (img) {
                 videoPreview.appendChild(img);
             }
         })
@@ -40,34 +52,35 @@ let renderThumbnail = function(videoPreview){
 
 // converts frame to image, taken from:
 // http://cwestblog.com/2017/05/03/javascript-snippet-get-video-frame-as-an-image/ 
-let getVideoImage = function(path, secs, callback) {
+let getVideoImage = function (path, secs, callback) {
     var me = this, video = document.createElement('video');
-    video.onloadedmetadata = function() {
-      if ('function' === typeof secs) {
-        secs = secs(this.duration);
-      }
-      this.currentTime = Math.min(Math.max(0, (secs < 0 ? this.duration : 0) + secs), this.duration);
+    video.onloadedmetadata = function () {
+        if ('function' === typeof secs) {
+            secs = secs(this.duration);
+        }
+        this.currentTime = Math.min(Math.max(0, (secs < 0 ? this.duration : 0) + secs), this.duration);
     };
-    video.onseeked = function(e) {
-      var canvas = document.createElement('canvas');
-      canvas.height = video.videoHeight;
-      canvas.width = video.videoWidth;
-      var ctx = canvas.getContext('2d');
-      ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-      var img = new Image();
-      img.src = canvas.toDataURL();
-      callback.call(me, img, this.currentTime, e);
+    video.onseeked = function (e) {
+        var canvas = document.createElement('canvas');
+        canvas.height = video.videoHeight;
+        canvas.width = video.videoWidth;
+        var ctx = canvas.getContext('2d');
+        ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+        var img = new Image();
+        img.src = canvas.toDataURL();
+        callback.call(me, img, this.currentTime, e);
     };
-    video.onerror = function(e) {
-      callback.call(me, undefined, undefined, e);
+    video.onerror = function (e) {
+        callback.call(me, undefined, undefined, e);
     };
     video.src = path;
-  }
+}
 
 
 
-window.onload = function(){
+window.onload = function () {
     let videos = getVideos();
+    hideVideos(videos);
     addThumbnails(videos);
 }
 
